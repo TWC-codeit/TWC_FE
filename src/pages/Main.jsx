c16394e36f5f3538bfcd433e90ff1bbc6ef94ee4
wnduswndus <wndus203@gmail.com>
2025-01-27 13:27:27 +0900

    [Feat/#5] 메인페이지 개발발

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import * as M from "../styles/MainStyle";
+import LogoImage from "../assets/icons/Logo.svg";
+import SearchIcon from "../assets/icons/search-sm.svg";
 
 function Main() {
     return (
         <>
-            <M.Main>메인 페이지</M.Main>
+            <M.Main>
+                {/*로고*/}
+                <M.Logo>
+                <img src={LogoImage} alt="TWINCLE Logo" />
+                </M.Logo>
+                {/*검색창*/}
+                <M.SearchBar>
+                    <M.SearchBox>
+                        <M.SearchIcon>
+                            <img src={SearchIcon} alt="Search Icon" />
+                        </M.SearchIcon>
+                        <M.SearchInput type="text" placeholder="Search by keyword" />
+                    </M.SearchBox>
+                </M.SearchBar>
+                {/*키워드*/}
+            </M.Main>
         </>
     );
 }
 
-export default Main;
+export default Main
--- src/styles/MainStyle.js
+++ src/styles/MainStyle.js
@@ -1,3 +1,74 @@
 import styled from "styled-components";
 
-export const Main = styled.div``;
+
+
+export const Main = styled.div`
+  width: 730px;
+  height: 280px; 
+  margin: 170px auto;
+  gap: 37px; 
+  box-sizing: border-box;
+`;
+
+
+export const Logo = styled.div`
+  img {
+    width: 355px; 
+    height: 65px;
+    display: block; 
+    margin: 0 auto;
+    margin-bottom: 45px; 
+  }
+`;
+export const SearchBar = styled.div`
+  width: 730px;
+  height: 76px;
+  padding: 15px 20px;
+  display: flex;
+  align-items: center;
+  border: 1px ;
+  border-radius: 10px;
+  background-color: #ffffff;
+  gap: 19px; 
+  box-shadow: 0px 0px 10px 0px #B6BFD1;
+  box-sizing: border-box;
+`;
+export const SearchBox = styled.div`
+  display: flex;
+  align-items: center;
+  width: 700px; 
+  height: 38px; 
+  gap: 19px;
+`;
+
+export const SearchIcon = styled.button`
+  width: 38px;
+  height: 38px;
+  border: none;
+  background-color: #ffffff; 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  img {
+    width: 28.5px;
+    height: 28.5px;
+  }
+`;
+
+export const SearchInput = styled.input`
+  flex-grow: 1;
+  height: 36px;  
+  border: none; 
+  outline: none;
+  font-family: "Pretendard"; 
+  font-weight: 500; 
+  font-size: 24px; 
+  line-height: 36px;
+  color: #161613;
+
+  ::placeholder {
+    color: #9a9a98; 
+    opacity: 0.39;
+  }
+`;
