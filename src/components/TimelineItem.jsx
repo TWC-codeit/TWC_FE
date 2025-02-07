4bd0c51e9be1bd10cb03590f06c48684ead3ccea
topeanut <ljhh1648@gmail.com>
2025-02-07 23:30:48 +0900

    [Feat#10]타임라이너디자인

---
+++ src/components/TimelineItem.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as T from "../styles/TimelineListStyle";
+
+const TimelineItem = ({ timeline }) => {
+  const navigate = useNavigate();
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <T.TimelineFolder
+      onClick={() => navigate(`/timeline/${timeline.id}`)}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      isHovered={isHovered}
+    >
+      <T.FolderTop isHovered={isHovered} />
+      <T.FolderContent>
+        <T.NewsLabel>News</T.NewsLabel>
+        <T.TimelineTitle>{timeline.title}</T.TimelineTitle>
+      </T.FolderContent>
+    </T.TimelineFolder>
+  );
+};
+
+export default TimelineItem;
