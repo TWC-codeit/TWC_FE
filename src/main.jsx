9f58a690a5eddefa69dd940169f69b9852142ffd
da2mon <87376242+da2mon@users.noreply.github.com>
2025-02-10 01:29:00 +0900

    Revert "[Feat] 스크랩 팝업 컴포넌트 구현"

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { CookiesProvider } from "react-cookie";
 import { createRoot } from "react-dom/client";
 import Header from "./components/Header.jsx";
 import App from "./App.jsx";
