b52c84cf27e77647909f19722a359efe3c0b38c2
mogg22 <ahrthdus1@gmail.com>
2025-02-12 02:37:40 +0900

    [Fix] TimelineList 토큰 처리 추가 및 import 경로 올바르게 수정

--- src/pages/TimelineList.jsx
+++ src/pages/TimelineList.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import TimelineItem from "../components/TimelineItem";
 import { Container, Title, TimelineWrapper, AddTimeline, NoData } from "../styles/TimelineStyle";
+import { getCookie } from "../api/Cookie.js";
+
+const apiUrl = import.meta.env.VITE_APP_API_URL;
 
 const TimelineList = () => {
   const [timelines, setTimelines] = useState([]);
@@ -9,20 +13,30 @@ const TimelineList = () => {
 
   useEffect(() => {
     const fetchTimelines = async () => {
+      const accessToken = getCookie("access_token");
+
+      if (!accessToken) {
+        console.error("JWT 토큰이 없습니다.");
+        return;
+      }
+
       try {
-        const response = await fetch("http://13.238.115.119/api/timelines");
-        if (!response.ok) {
-          throw new Error("Failed to fetch timelines");
-        }
-        const data = await response.json();
-        setTimelines(data);
+        const response = await axios.get(`${apiUrl}/timelines`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        setTimelines(response.data.timelines);
       } catch (error) {
-        console.error("Error fetching timelines:", error);
+        console.error("타임라인 불러오기 실패:", error);
       }
     };
 
     fetchTimelines();
   }, []);
+
+  console.log(timelines);
 
   return (
     <Container>
