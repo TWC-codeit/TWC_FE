9e041f79d711ce30bdaff7a7e69fb2a518582bbb
mogg22 <ahrthdus1@gmail.com>
2025-02-14 14:25:08 +0900

    [Fix] privaterouter 토큰 처리 오류 해결

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -3,7 +3,8 @@ import { Navigate } from "react-router-dom";
 import { getCookie } from "../api/Cookie.js";
 
 const PrivateRoute = ({ element }) => {
-  const token = getCookie("accessToken");
+  const token = getCookie("access_token");
+  
 
   return token ? element : <Navigate to="/login" />;
 };
