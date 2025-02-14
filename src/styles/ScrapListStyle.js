3320fd6e248447ef50f1c451db3140c81c218573
DaheeJeong <d.hee@ewhain.net>
2025-02-14 14:46:29 +0900

    [Design] 페이지 최대 너비 삭제 및 여백 추가

--- src/styles/ScrapListStyle.js
+++ src/styles/ScrapListStyle.js
@@ -2,9 +2,8 @@ import styled from "styled-components";
 import palette from "../lib/colorPalette.js";
 
 export const Container = styled.div`
-  padding: 0 5%;
+  padding: 0 10%;
   background-color: ${palette.background};
-  max-width: 1440px; /* 콘텐츠의 최대 너비 */
   margin: 0 auto; /* 양쪽 여백을 동일하게 하여 가운데 정렬 */
   width: 100%; /* 전체 너비를 100%로 설정 */
   box-sizing: border-box;
